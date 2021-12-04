1e493bec2a2fb733f8ab47450db7dfbd82861f61
santiago tobon <43623235+santitobon9@users.noreply.github.com>
2021-12-03 17:16:22 -0800

    Reformatted navbar.js with prettier extension
    Changed tab widths to 2 spaces, changed line width to 120 and remove extra whitespaces.

--- src/navbar.js
+++ src/navbar.js
@@ -3,72 +3,77 @@ import { NavLink } from "react-router-dom";
 import Api from "./api";
 
 class NavBar extends Component {
-	toggleNavigation() {
-		window.click_toggle();
-	}
+  toggleNavigation() {
+    window.click_toggle();
+  }
 
-	render() {
-		return (
-			<nav className="navbar navbar-default navbar-fixed">
-				<div className="container-fluid">
-					<div className="navbar-header">
-						<NavLink className="navbar-brand" to={`${process.env.PUBLIC_URL}/home`}>
-							Battlecode 2021
-						</NavLink>
-					</div>
-					<div className="collapse navbar-collapse">
-						<NavBarAccount />
-					</div>
-				</div>
-			</nav>
-		);
-	}
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <NavLink
+              className="navbar-brand"
+              to={`${process.env.PUBLIC_URL}/home`}
+            >
+              Battlecode 2021
+            </NavLink>
+          </div>
+          <div className="collapse navbar-collapse">
+            <NavBarAccount />
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 class NavBarAccount extends Component {
-	constructor() {
-		super();
-		this.state = { logged_in: false };
-	}
+  constructor() {
+    super();
+    this.state = { logged_in: false };
+  }
 
-	componentDidMount() {
-		Api.loginCheck((logged_in) => {
-			this.setState({ logged_in });
-		});
-	}
+  componentDidMount() {
+    Api.loginCheck((logged_in) => {
+      this.setState({ logged_in });
+    });
+  }
 
-	logout() {
-		Api.logout(() => {
-			this.setState({ logged_in: false });
-			window.location.reload();
-		});
-		window.location.reload();
-	}
+  logout() {
+    Api.logout(() => {
+      this.setState({ logged_in: false });
+      window.location.reload();
+    });
+    window.location.reload();
+  }
 
-	render() {
-		if (this.state.logged_in === true) {
-			return (
-				<ul className="nav navbar-nav navbar-right">
-					<li>
-						<a onClick={this.logout}>Log out</a>
-					</li>
-				</ul>
-			);
-		}
-		if (this.state.logged_in === false) {
-			return (
-				<ul className="nav navbar-nav navbar-right">
-					<li>
-						<NavLink to={`${process.env.PUBLIC_URL}/register`}>Register</NavLink>
-					</li>
-					<li>
-						<NavLink to={`${process.env.PUBLIC_URL}/login`}>Log in</NavLink>
-					</li>
-				</ul>
-			);
-		}
-		return <ul />;
-	}
+  render() {
+    if (this.state.logged_in === true) {
+      return (
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <a onClick={this.logout}>Log out</a>
+          </li>
+        </ul>
+      );
+    }
+    if (this.state.logged_in === false) {
+      return (
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <NavLink to={`${process.env.PUBLIC_URL}/register`}>
+              Register
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={`${process.env.PUBLIC_URL}/login`}>Log in</NavLink>
+          </li>
+        </ul>
+      );
+    }
+    return <ul />;
+  }
 }
 
 export default NavBar;
