77d1707bdefbe01a91fac17b802b8e21b2d31448
santiago tobon <43623235+santitobon9@users.noreply.github.com>
2021-12-03 17:20:16 -0800

    Reformatted sidebar.js with prettier extension
    Changed tab widths to 2 spaces, changed line width to 120 and remove extra whitespaces.

--- src/sidebar.js
+++ src/sidebar.js
@@ -4,129 +4,186 @@ import Api from "./api";
 import * as Cookies from "js-cookie";
 
 class NLink extends Component {
-	render() {
-		return (
-			<li>
-				<NavLink {...this.props} activeStyle={{ opacity: 1, fontWeight: 800 }} />
-			</li>
-		);
-	}
+  render() {
+    return (
+      <li>
+        <NavLink
+          {...this.props}
+          activeStyle={{ opacity: 1, fontWeight: 800 }}
+        />
+      </li>
+    );
+  }
 }
 
 class SideBar extends Component {
-	constructor() {
-		super();
-		this.state = { on_team: false, logged_in: false, user: {}, league: {} };
-	}
+  constructor() {
+    super();
+    this.state = { on_team: false, logged_in: false, user: {}, league: {} };
+  }
 
-	componentDidMount() {
-		Api.loginCheck((logged_in) => {
-			this.setState({ logged_in });
-		});
-		Api.getUserTeam(
-			function (e) {
-				this.setState({ on_team: e !== null });
-			}.bind(this)
-		);
-		if (Cookies.get("teamName") === "" && Cookies.get("teamKey") === "") {
-			this.setState({ on_team: false });
-		}
-	}
+  componentDidMount() {
+    Api.loginCheck((logged_in) => {
+      this.setState({ logged_in });
+    });
+    Api.getUserTeam(
+      function (e) {
+        this.setState({ on_team: e !== null });
+      }.bind(this)
+    );
+    if (Cookies.get("teamName") === "" && Cookies.get("teamKey") === "") {
+      this.setState({ on_team: false });
+    }
+  }
 
-	isSubmissionEnabled() {
-		if (this.state.user.is_staff === true) {
-			return true;
-		}
-		if (this.state.league.game_released === true) {
-			return true;
-		}
-		return true;
-	}
+  isSubmissionEnabled() {
+    if (this.state.user.is_staff === true) {
+      return true;
+    }
+    if (this.state.league.game_released === true) {
+      return true;
+    }
+    return true;
+  }
 
-	// for icon options below, see https://themes-pixeden.com/font-demos/7-stroke/
-	render() {
-		return (
-			<div className="sidebar" data-color="dust">
-				{" "}
-				{/* data-color is defined in light-bootstrap-dashboard.css */}
-				<div className="sidebar-wrapper">
-					<div className="logo">
-						<a href="/">
-							<img alt="logo" src="../assets/img/logo.png" />
-						</a>
-						<p>Battlecode 2021</p>
-					</div>
-					<ul className="nav nav-pills nav-stacked">
-						<NLink to={`#`} style={{ visibility: "hidden" }}></NLink>
-						<NLink to={`${process.env.PUBLIC_URL}/home`}>
-							<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-								<i className="pe-7s-home pe-fw" />
-								Home
-							</p>
-						</NLink>
-						<NLink to={`${process.env.PUBLIC_URL}/getting-started`}>
-							<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-								<i className="pe-7s-sun pe-fw" />
-								Getting Started
-							</p>
-						</NLink>
-						<NLink to={`${process.env.PUBLIC_URL}/resources`}>
-							<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-								<i className="pe-7s-note2 pe-fw" />
-								Resources
-							</p>
-						</NLink>
-						<NLink to={`${process.env.PUBLIC_URL}/tournaments`}>
-							<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-								<i className="pe-7s-medal pe-fw" />
-								Tournaments
-							</p>
-						</NLink>
-						{this.state.logged_in && (
-							<NLink to={`${process.env.PUBLIC_URL}/team`}>
-								<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-									<i className="pe-7s-users pe-fw" />
-									Team
-								</p>
-							</NLink>
-						)}
-						<NLink to={`${process.env.PUBLIC_URL}/rankings`}>
-							<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-								<i className="pe-7s-graph1 pe-fw" />
-								Rankings
-							</p>
-						</NLink>
-						{this.state.on_team && (
-							<NLink to={`${process.env.PUBLIC_URL}/submissions`}>
-								<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-									<i className="pe-7s-up-arrow pe-fw" />
-									Submissions
-								</p>
-							</NLink>
-						)}
-						{this.state.on_team && this.isSubmissionEnabled() && (
-							<NLink to={`${process.env.PUBLIC_URL}/scrimmaging`}>
-								<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-									<i className="pe-7s-joy pe-fw" />
-									Scrimmaging
-								</p>
-							</NLink>
-						)}
-						<br />
-						{this.state.user.is_staff && (
-							<NLink to={`${process.env.PUBLIC_URL}/staff`}>
-								<p style={{ fontWeight: "inherit", textTransform: "none", fontSize: "inherit" }}>
-									<i className="pe-7s-tools pe-fw" />
-									Staff
-								</p>
-							</NLink>
-						)}
-						<br />
-					</ul>
-				</div>
-			</div>
-		);
-	}
+  // for icon options below, see https://themes-pixeden.com/font-demos/7-stroke/
+  render() {
+    return (
+      <div className="sidebar" data-color="dust">
+        {" "}
+        {/* data-color is defined in light-bootstrap-dashboard.css */}
+        <div className="sidebar-wrapper">
+          <div className="logo">
+            <a href="/">
+              <img alt="logo" src="../assets/img/logo.png" />
+            </a>
+            <p>Battlecode 2021</p>
+          </div>
+          <ul className="nav nav-pills nav-stacked">
+            <NLink to={`#`} style={{ visibility: "hidden" }}></NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/home`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-home pe-fw" />
+                Home
+              </p>
+            </NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/getting-started`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-sun pe-fw" />
+                Getting Started
+              </p>
+            </NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/resources`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-note2 pe-fw" />
+                Resources
+              </p>
+            </NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/tournaments`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-medal pe-fw" />
+                Tournaments
+              </p>
+            </NLink>
+            {this.state.logged_in && (
+              <NLink to={`${process.env.PUBLIC_URL}/team`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-users pe-fw" />
+                  Team
+                </p>
+              </NLink>
+            )}
+            <NLink to={`${process.env.PUBLIC_URL}/rankings`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-graph1 pe-fw" />
+                Rankings
+              </p>
+            </NLink>
+            {this.state.on_team && (
+              <NLink to={`${process.env.PUBLIC_URL}/submissions`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-up-arrow pe-fw" />
+                  Submissions
+                </p>
+              </NLink>
+            )}
+            {this.state.on_team && this.isSubmissionEnabled() && (
+              <NLink to={`${process.env.PUBLIC_URL}/scrimmaging`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-joy pe-fw" />
+                  Scrimmaging
+                </p>
+              </NLink>
+            )}
+            <br />
+            {this.state.user.is_staff && (
+              <NLink to={`${process.env.PUBLIC_URL}/staff`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-tools pe-fw" />
+                  Staff
+                </p>
+              </NLink>
+            )}
+            <br />
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SideBar;
